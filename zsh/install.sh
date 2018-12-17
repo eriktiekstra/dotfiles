4c9ffad9c7b6a71d2141da80022fe1bbf93510f9
Erik Tiekstra <erik@tiekstra.se>
2018-12-17 13:06:17 +0000

    Add new file

---
+++ zsh/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# oh-my-zsh
+#
+# This installs oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh)
+
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
