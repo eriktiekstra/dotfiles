c1afdcf73387a3678bb11108c1b54c38bee4f2ce
Erik Tiekstra <erik@tiekstra.se>
2018-12-17 10:46:21 +0000

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,16 @@
 alias reload!='. ~/.zshrc'
 
-alias cls='clear' # Good 'ol Clear Screen command
+alias cl='clear' # Good 'ol Clear Screen command
+
+alias home='cd ~/'
+alias ls='ls -la'
+alias sites='cd ~/Sites/'
+
+#alias vhosts='sudo nano /etc/apache2/extra/httpd-vhosts.conf'
+
+alias serve='python -m SimpleHTTPServer 8000'
+
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+
+alias myip='echo "External: $(curl -s http://ipecho.net/plain) \nInternal: $(ipconfig getifaddr en0)"'
+
