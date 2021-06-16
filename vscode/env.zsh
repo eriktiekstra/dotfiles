6e5415b7ca0714b13a2368a31697c51fdaec0b48
Erik Tiekstra <eriktiekstra@users.noreply.github.com>
2021-06-16 11:45:36 +0200

    Create env.zsh

---
+++ vscode/env.zsh
@@ -0,0 +1,6 @@
+# To be able to use the Visual Studio Marketplace with VSCodium (Some extensions are not available through Open VSX)
+export VSCODE_GALLERY_SERVICE_URL='https://marketplace.visualstudio.com/_apis/public/gallery'
+export VSCODE_GALLERY_CACHE_URL='https://vscode.blob.core.windows.net/gallery/index'
+export VSCODE_GALLERY_ITEM_URL='https://marketplace.visualstudio.com/items'
+export VSCODE_GALLERY_CONTROL_URL=''
+export VSCODE_GALLERY_RECOMMENDATIONS_URL=''
